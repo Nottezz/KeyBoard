051b0cb2f618bd3fd14130dfdc243347a3332a5a
Nikita <116362682+R00kie-dot@users.noreply.github.com>
2025-02-23 21:44:00 +0300

    Adding Django debug toolbar (#1)
    * add django debug toolbar
    
    * linter reformatted, fix actions name

--- keyboard_project/keyboard_project/settings.py
+++ keyboard_project/keyboard_project/settings.py
@@ -30,6 +30,9 @@ SECRET_KEY = config["settings"]["SECRET_KEY"]
 DEBUG = True
 
 ALLOWED_HOSTS = []
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
 
 
 # Application definition
@@ -41,9 +44,11 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "debug_toolbar",
 ]
 
 MIDDLEWARE = [
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -52,6 +57,7 @@ MIDDLEWARE = [
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
+
 
 ROOT_URLCONF = "keyboard_project.urls"
 
