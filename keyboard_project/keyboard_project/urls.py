62d9406a926e5d45436c2f7c71d935540bdf2bb2
Nikita Yakovlev <driftworks2013@yandex.ru>
2025-03-15 15:46:49 +0300

    path to media if debug

--- keyboard_project/keyboard_project/urls.py
+++ keyboard_project/keyboard_project/urls.py
@@ -16,12 +16,18 @@ Including another URLconf
 """
 
 from debug_toolbar.toolbar import debug_toolbar_urls
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
+from keyboard_project import settings
+
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("kb_app.urls", namespace="keyboards"))
+    path("", include("kb_app.urls", namespace="keyboards")),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 urlpatterns.extend(debug_toolbar_urls())
