9a2f296e061b620e0529821e51d04900f5863883
Nikita Yakovlev <driftworks2013@yandex.ru>
2025-03-15 15:45:16 +0300

    new paths

--- keyboard_project/kb_app/urls.py
+++ keyboard_project/kb_app/urls.py
@@ -6,4 +6,10 @@ app_name = "keyboards"
 
 urlpatterns = [
     path("", views.Home.as_view(), name="home"),
+    path("catalog/", views.KeyBoardList.as_view(), name="keyboard_list"),
+    path(
+        "catalog/<slug:keyboard_slug>/",
+        views.KeyBoardDetails.as_view(),
+        name="keyboard_details",
+    ),
 ]
