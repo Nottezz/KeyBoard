ee0dfbd58b744424e10407c709d7caa4a8f2141a
Nikita Yakovlev <driftworks2013@yandex.ru>
2025-03-16 23:10:27 +0300

    rename source dir

--- src/kb_app/tests.py
+++ src/kb_app/tests.py
@@ -4,6 +4,11 @@ from django.urls import reverse
 
 class GetPagesTestCase(TestCase):
     def test_main_page(self):
-        path = reverse("home")
+        path = reverse("keyboards:home")
+        response = self.client.get(path)
+        self.assertEqual(200, response.status_code)
+
+    def test_catalog_page(self):
+        path = reverse("keyboards:keyboard_list")
         response = self.client.get(path)
         self.assertEqual(200, response.status_code)
