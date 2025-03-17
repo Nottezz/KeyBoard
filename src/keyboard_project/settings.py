da16bd84cb07a9ecaa16bd8cd91273c862f5c357
Nikita Yakovlev <driftworks2013@yandex.ru>
2025-03-17 08:50:41 +0300

    base dir for settings.ini

--- src/keyboard_project/settings.py
+++ src/keyboard_project/settings.py
@@ -15,9 +15,6 @@ from configparser import ConfigParser
 from pathlib import Path
 
 
-config = ConfigParser()
-config.read("settings.ini")
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -25,6 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+
+config = ConfigParser()
+config.read(BASE_DIR / "settings.ini")
+
 SECRET_KEY = config["settings"]["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
